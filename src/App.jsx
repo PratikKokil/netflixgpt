ad32358d57f151007e9bc69a7bf8d94b69a90cb0
Pratik Kokil <kokilpratik220@gmail.com>
2025-05-05 15:01:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './index.css'
+function App() {
+   
+
+  return (
+    
+      <div className=' bg-red-600'>
+        Lets build Netflixgpt
+      </div>
+      
+  )
+}
+
+export default App;
